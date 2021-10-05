5fb1afcae6b5e1769146a62b9f9e3cfd35d2ed46
sdntmn <67434735+sdntmn@users.noreply.github.com>
2021-10-06 01:13:25 +0500

    fix review 1
    Исправление и доработка кода после первого ревью

--- controllers/controllerСards.js
+++ controllers/controllerСards.js
@@ -4,20 +4,30 @@ const Card = require("../models/modelСard");
 // Обрабатываем запрос на получение данных всех Cards ======================
 module.exports.getCards = (req, res) => {
   return Card.find({})
-    .then((cards) => res.status(200).send(cards))
+    .then((cards) => {
+      res.status(200).send(cards);
+    })
 
     .catch((err) => {
-      console.log("Ошибка:" + err);
+      console.log(`Ошибка: ${err}`);
       res.status(500).send({ message: "Ошибка!!!" });
     });
 };
 
-// Обрабатываем запрос на удаление Card ===========
+// +Обрабатываем запрос на удаление Card ===================================
 module.exports.deleteCard = (req, res) => {
   return Card.findByIdAndRemove({ _id: req.body._id, owner: req.user._id })
-    .then((card) => res.status(200).send(card))
+    .then((card) => {
+      console.log(card);
+      if (!card) {
+        return res
+          .status(404)
+          .send({ message: "Карточка с указанным _id не найдена." });
+      }
+      return res.status(200).send(card);
+    })
     .catch((err) => {
-      console.log("Ошибка:" + err);
+      console.log(`Ошибка: ${err}`);
       res.status(500).send({ message: "Ошибка!!!" });
     });
 };
@@ -31,60 +41,77 @@ module.exports.createCard = (req, res) => {
       return res.status(200).send(card);
     })
     .catch((err) => {
-      if (err.name == "ValidationError") {
+      if (err.name === "ValidationError") {
         return res.status(400).send({
           message: `${Object.values(err.errors)
-            .map((error) => error.message)
+            .map((error) => {
+              return error.message;
+            })
             .join(", ")}`,
         });
       }
-      console.log("Error:" + err);
+      console.log(`Ошибка: ${err}`);
       return res.status(500).send({ message: "Ошибка!!!" });
     });
 };
 
 // Обрабатываем запрос на добавление Лайк Card==============================
-module.exports.likeCard = (req, res) =>
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true }
-  )
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, {
+    $addToSet: { likes: req.user._id },
+  })
     .then((data) => {
       console.log(data);
+      if (!data) {
+        return res
+          .status(404)
+          .send({ message: "Передан несуществующий _id карточки." });
+      }
       return res.status(200).send(data);
     })
     .catch((err) => {
-      if (err.name == "ValidationError") {
+      if (err.name === "ValidationError") {
         return res.status(400).send({
           message: `${Object.values(err.errors)
-            .map((error) => error.message)
+            .map((error) => {
+              return error.message;
+            })
             .join(", ")}`,
         });
       }
-      console.log("Error:" + err);
+      console.log(`Ошибка: ${err}`);
       return res.status(500).send({ message: "Ошибка!!!" });
     });
+};
 
 // Обрабатываем запрос на удаление Лайк Card================================
-module.exports.dislikeCard = (req, res) =>
+module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { $pull: { likes: req.user._id } },
     { new: true }
   )
     .then((data) => {
       console.log(data);
+      if (!data) {
+        return res
+          .status(404)
+          .send({ message: "Передан несуществующий _id карточки." });
+      }
       return res.status(200).send(data);
     })
+
     .catch((err) => {
-      if (err.name == "ValidationError") {
+      if (err.name === "ValidationError") {
         return res.status(400).send({
           message: `${Object.values(err.errors)
-            .map((error) => error.message)
+            .map((error) => {
+              return error.message;
+            })
             .join(", ")}`,
         });
       }
-      console.log("Error:" + err);
+      console.log(`Ошибка: ${err}`);
       return res.status(500).send({ message: "Ошибка!!!" });
     });
+};
