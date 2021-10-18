05d1907f2686df1da19e5a7881a43ecded44dd63
sdntmn <67434735+sdntmn@users.noreply.github.com>
2021-10-19 01:06:59 +0500

    fix
    Централизованная обработка ошибок, валидация запросов. Мозг уже взрывается

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,45 +5,34 @@ const jwt = require("jsonwebtoken");
 // Импортируем модель user из ../models/user
 const User = require("../models/userModel");
 
+const NotFoundError = require("../errors/not-found-err");
+
 // Обрабатываем запрос на получение данных всех Users ======================
-module.exports.getUsers = (req, res) => {
+module.exports.getUsers = (req, res, next) => {
   return User.find({})
     .then((user) => {
       return res.status(200).send(user);
     })
 
-    .catch((err) => {
-      console.log(`Ошибка: ${err}`);
-      res.status(500).send({ message: err });
-    });
+    .catch(next);
 };
 
 // Обрабатываем запрос на получение данных конкретного User по id===========
-module.exports.getUser = (req, res) => {
+module.exports.getUser = (req, res, next) => {
   return User.findById(req.params.id)
     .then((user) => {
       if (!user) {
-        return res
-          .status(404)
-          .send({ message: " Пользователь по указанному _id не найден" });
+        throw new NotFoundError({
+          message: " Пользователь по указанному _id не найден",
+        });
       }
       return res.status(200).send(user);
     })
-    .catch((err) => {
-      // console.log(err);
-      if (err.name === "CastError") {
-        console.log(`Ошибка: ${err}`);
-        res.status(400).send({ message: "Запрос ошибка 1" });
-      }
-      if (res) {
-        console.log(`Ошибка: ${err}`);
-        res.send({ message: "Ошибка !!!" });
-      }
-    });
+    .catch(next);
 };
 
 // Обрабатываем запрос на создание User=====================================
-module.exports.createUser = (req, res) => {
+module.exports.createUser = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
@@ -56,19 +45,14 @@ module.exports.createUser = (req, res) => {
       });
     })
     .then((user) => {
+      console.log(user);
       return res.status(200).send(user);
     })
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        return res.status(400).send(`message: ${err}`);
-      }
-      console.log(`Ошибка: ${err}`);
-      return res.status(500).send(`Ошибка: ${err}`);
-    });
+    .catch(next);
 };
 
 // Обрабатываем запрос на обновление данных User ===========================
-module.exports.updateUser = (req, res) => {
+module.exports.updateUser = (req, res, next) => {
   return User.findByIdAndUpdate(
     req.user._id,
     { ...req.body },
@@ -77,26 +61,11 @@ module.exports.updateUser = (req, res) => {
     .then((user) => {
       return res.status(200).send(user);
     })
-    .catch((err) => {
-      if (err.name === "CastError") {
-        console.log(`Ошибка: ${err}`);
-        res
-          .status(400)
-          .send({ message: "Пользователь с указанным _id не найден." });
-      }
-      if (err.name === "ValidationError") {
-        console.log(`Ошибка: ${err}`);
-        res.status(400).send({
-          message: "Переданы некорректные данные при обновлении профиля.",
-        });
-      }
-      console.log(`Ошибка: ${err}`);
-      return res.status(500).send({ message: "Ошибка!!!" });
-    });
+    .catch(next);
 };
 
 // Обрабатываем запрос на обновление Avatar User ===========================
-module.exports.updateUserAvatar = (req, res) => {
+module.exports.updateUserAvatar = (req, res, next) => {
   return User.findByIdAndUpdate(
     req.user._id,
     { ...req.body },
@@ -105,51 +74,30 @@ module.exports.updateUserAvatar = (req, res) => {
     .then((user) => {
       return res.status(200).send(user);
     })
-    .catch((err) => {
-      if (err.name === "CastError") {
-        console.log(`Ошибка: ${err}`);
-        res
-          .status(404)
-          .send({ message: "Пользователь с указанным _id не найден." });
-      }
-      if (err.name === "ValidationError") {
-        console.log(`Ошибка: ${err}`);
-        res.status(400).send({
-          message: "Переданы некорректные данные при обновлении аватара.",
-        });
-      }
-      console.log(`Ошибка: ${err}`);
-      return res.status(500).send({ message: "Ошибка!!!" });
-    });
+    .catch(next);
 };
 
-module.exports.login = (req, res) => {
+module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, "some-secret-key", {
         expiresIn: "7d",
       });
-      console.log(token);
       if (!user) {
         return Promise.reject(new Error("Неправильные почта или пароль"));
       }
       return res.send({ token });
     })
-    .catch((err) => {
-      res.status(401).send({ message: err.message });
-    });
+    .catch(next);
 };
 
 // Обрабатываем запрос на получение данных авторизированного Usera =========
-module.exports.getAuthUser = (req, res) => {
+module.exports.getAuthUser = (req, res, next) => {
   const id = req.params.userId;
   return User.findById({ _id: id })
     .then((user) => {
       return res.status(200).send(user);
     })
-    .catch((err) => {
-      console.log(`Ошибка: ${err}`);
-      res.status(500).send({ message: err });
-    });
+    .catch(next);
 };
